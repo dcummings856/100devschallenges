1a49f019e495f1dd7fb18322005bd89b61f15135
David <dcummings856@gmail.com>
2021-01-12 19:10:45 -0500

    first challenge

---
+++ 1-11-21.js
@@ -0,0 +1,11 @@
+function takeTwo(num1, num2) {
+  console.log(Math.floor(num1 / num2))
+}
+
+takeTwo(4, 5)
+takeTwo(1992, 2342)
+takeTwo(42, 3)
+takeTwo(3, 3231)
+takeTwo(92, 70)
+takeTwo(14, 6)
+takeTwo(23, 56)
