27751b1c026ec17d2bfc29a6be1d7a80615c36bf
David <dcummings856@gmail.com>
2021-01-14 09:51:40 -0500

    1-12 and 1-13 challenge

---
+++ 1-13-21.js
@@ -0,0 +1,8 @@
+//Please create a function that takes in an array. The function should console.log() the sum of the first value in the array and the last value in the array
+
+function takeInArr(arr) {
+  console.log(arr[0])
+  console.log(arr[arr.length - 1])
+}
+
+takeInArr(['hi', 'howdy', 'holla'])
